5fc79b548345a7d7b0d3cf27f1b259ce3053eb5a
Arseny Burov <arsenyburov@gmail.com>
2020-06-28 02:17:05 +0300

    l05BADABUTTSSSS

--- lesson05/src/ru/geekbrains/lesson05/Main.java
+++ lesson05/src/ru/geekbrains/lesson05/Main.java
@@ -3,6 +3,7 @@ package ru.geekbrains.lesson05;
 public class Main {
 
     public static void main(String[] args) {
-      Methods.methodOne();
+        Methods.methodOne();
+        Methods.methodTwo();
     }
 }
